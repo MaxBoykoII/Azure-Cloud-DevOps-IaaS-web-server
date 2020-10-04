ec9c984bd2726cbdcc5939265aae576807f86d57
Max Boyko <maxboykomax@gmail.com>
2020-10-03 18:32:28 -0700

    feat: Add tagging policy assignment

--- main.tf
+++ main.tf
@@ -1,11 +1,44 @@
 # Configure the Azure Provider
 provider "azurerm" {
-    version = "=2.20.0"
-    features {}
+  version = "=2.20.0"
+  features {}
+}
+
+# Add a datasource for the current subscription
+data "azurerm_subscription" "current" {
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "iaas-web-server" {
-    name = "iass-web-server-resources"
-    location = "West US"
+  name     = "iass-web-server-resources"
+  location = "West US"
+}
+
+# Create a policy definition to enforce tagging resources
+resource "azurerm_policy_definition" "tagging_policy" {
+  name         = "deny-indexed-resources-without-tags"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "Deny Creation of Indexed Resources without Tags"
+  description  = "Ensures that all indexed resources are tagged. This will help us with organization and tracking, and make it easier to log when things go wrong."
+  policy_rule  = <<POLICY_RULE
+  {
+      "if": {
+          "field": "tags",
+          "exists": "false"
+      },
+      "then": {
+          "effect": "deny"
+      }
+  }
+  POLICY_RULE
+}
+
+# Create a policy assignment for the tagging policy
+resource "azurerm_policy_assignment" "tagging_policy_assignment" {
+  name                 = "tagging-policy"
+  scope                = data.azurerm_subscription.current.id
+  policy_definition_id = azurerm_policy_definition.tagging_policy.id
+  description          = "Assignment of the tagging policy"
+  display_name         = "tagging-policy"
 }
