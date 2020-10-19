0d02a477057f31ce6bd9005fdf2e8586a78a6afa
Max Boyko <maxboykomax@gmail.com>
2020-10-19 01:27:03 -0700

    Update the packer template so that the provisioned VMs automatically start the server

--- main.tf
+++ main.tf
@@ -124,6 +124,21 @@ resource "azurerm_network_security_rule" "inbound" {
   network_security_group_name = azurerm_network_security_group.main.name
 }
 
+# Allow inbound traffic to the loadbalancer
+resource "azurerm_network_security_rule" "inbound2" {
+  name                        = "${var.prefix}-loadbalancer-inbound-2"
+  priority                    = 115
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
 # Allow outbound traffic to the loadbalancer
 resource "azurerm_network_security_rule" "outbound" {
   name                        = "${var.prefix}-loadbalancer-outbound"
@@ -133,6 +148,20 @@ resource "azurerm_network_security_rule" "outbound" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
+resource "azurerm_network_security_rule" "outbound2" {
+  name                        = "${var.prefix}-loadbalancer-outbound-2"
+  priority                    = 105
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.main.name
